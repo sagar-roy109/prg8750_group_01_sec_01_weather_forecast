76c5833e594a4d06aa70bb135c8263264746cc8d
Tejaswini <tejaswininallapaneni26@gmail.com>
2023-02-14 12:46:09 -0500

    test

---
+++ src/pages/Test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+import BlogList from '../components/BlogList';
+
+const Test = () => {
+  return (
+    <>
+      <Header></Header>
+      <BlogList></BlogList>
+    </>
+  );
+};
+
+export default Test;
