11a5244cdba1f279dfdb4389bf9e6fd250caadda
sagar-roy109 <sagar.roy.freelancer@gmail.com>
2023-02-13 21:31:11 -0500

    Routing and blog page added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import SearchTemp from './components/SearchTemp';
-import Header from './components/Header';
-
-
-
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
+import Blogs from "./pages/Blogs";
 
 const api = {
   key: "42a11fd3bfecf2a59e5faa5d5e9c5f94",
-  base: "https://api.openweathermap.org/data/2.5/"
-}
-
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
-
-
   return (
     <>
-			<Header></Header>
-			<SearchTemp></SearchTemp>
-		</>
-)};
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blogs" element={<Blogs />} />
+      </Routes>
+    </>
+  );
+}
 
 export default App;
