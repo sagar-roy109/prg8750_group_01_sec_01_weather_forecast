e8194952432b3a6a06c7b3e602d1d493e558904d
Tejaswini <tejaswininallapaneni26@gmail.com>
2023-03-28 13:13:28 -0400

    commit

--- model/postmodel.js
+++ model/postmodel.js
@@ -1,11 +1,1 @@
-const mongoose = require('mongoose');
 
-const postSchema = new mongoose.Schema({
-  img: String,
-  DOP: String,
-  title: String,
-  posttype: String,
-});
-
-const post = mongoose.model('post', postSchema);
-module.exports = post;
